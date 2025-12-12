9d5af87273f9b34e1bb30939726ccb647b301394
OlivakarSankalp123 <sankalpolivakar86@gmail.com>
2025-12-12 19:02:59 +0530

    Second Commit

--- src/main/java/com/docker_test/controller/MessageController.java
+++ src/main/java/com/docker_test/controller/MessageController.java
@@ -1,13 +1,18 @@
 package com.docker_test.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
 
-    @RequestMapping("/message")
+    @GetMapping("/message")
     public String getMessage(){
         return "Message";
+    }
+
+    @GetMapping("/sankalp")
+    public String getSankalp(){
+        return "Hii Sankalp";
     }
 }
